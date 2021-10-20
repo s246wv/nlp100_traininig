b2ed024d40532f8b618e73a201aad21230f98da0
s246wv <satoshi.nishimura.3194@gmail.com>
2021-10-20 21:17:34 +0900

    add _06.py

--- chapter_1/_06.py
+++ chapter_1/_06.py
@@ -3,15 +3,25 @@
 “paraparaparadise”と”paragraph”に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．さらに，’se’というbi-gramがXおよびYに含まれるかどうかを調べよ．
 '''
 
-#import _05
-from _05 import ngram
+def main():
+    from _05 import ngram
 
-strX = "paraparaparadise"
-strY = "paragraph"
+    strX = "paraparaparadise"
+    strY = "paragraph"
 
-X = ngram(strX,2,"char")
-Y = ngram(strY,2,"char")
+    X = set(ngram(strX,2,"char"))
+    Y = set(ngram(strY,2,"char"))
 
-#print(X)
-#print(Y)
-#print(X and Y)
+    print(X)
+    print(Y)
+    print(X | Y)
+    print(X & Y)
+    print(X - Y)
+    print(Y - X)
+
+    se = {"se"}
+    print(se.issubset(X))
+    print(se.issubset(Y))
+
+if __name__ == "__main__":
+    main()
