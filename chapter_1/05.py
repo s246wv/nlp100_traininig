a311ac6e0af60c8b2955407614f73f28295264b2
s246wv <satoshi.nishimura.3194@gmail.com>
2021-10-20 20:45:40 +0900

    revise 05.py

--- chapter_1/05.py
+++ chapter_1/05.py
@@ -6,12 +6,32 @@
 def ngram(str,n,att):
     ret = []
     if att == "word":
-        
+        wind = 0
+        # 単語は数字で区切られているという勝手な前提。
+        words = str.split(" ")
+        for word in words:
+            if wind != len(words)-(n-1):
+                gram = ""
+                for i in range(0,n):
+                    gram += words[wind+i]
+                    if i != n-1:
+                        gram += " "
+                ret.append(gram)
+                wind += 1
     elif att == "char":
+        sind = 0
         for s in str:
-            if str.index(s) != len(str)-1:
-                ret.append(s+)
+            if sind != len(str)-(n-1):
+                gram = ""
+                for i in range(0,n):
+                    gram += str[sind+i]
+                ret.append(gram)
+                sind += 1
     else:
         print("Please provide the third variable as \"word\" or \"char\".")
 
+    return ret
+
 str = "I am an NLPer"
+# print(ngram(str,2,"word"))
+print(ngram(str,2,"char"))
