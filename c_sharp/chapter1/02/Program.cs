af0c4d0b322bda9ee42d6351c4ff878f7ac6a827
s246wv <satoshi.nishimura.3194@gmail.com>
2021-11-22 13:44:36 +0900

    これでいいのだろうか？

---
+++ c_sharp/chapter1/02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+02. 「パトカー」＋「タクシー」＝「パタトクカシーー」Permalink
+「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+*/
+using System;
+using System.Text;
+
+string str1 = "パトカー";
+string str2 = "タクシー";
+StringBuilder sb = new StringBuilder();
+for(int i=0; i<str1.Length; i++){
+    sb.Append(str1[i]);
+    sb.Append(str2[i]);
+}
+string ret = sb.ToString();
+
+Console.WriteLine(ret);
