1be70b947db829afd54bee027663919d249ef915
s246wv <satoshi.nishimura.3194@gmail.com>
2021-11-22 13:58:41 +0900

    いまいちまだなれない．もっとスマートな方法がありそうなのだけれど．

---
+++ c_sharp/chapter1/03/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+03. 円周率Permalink
+“Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.”という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+*/
+using System;
+using System.Text;
+
+string str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.";
+string[] terms = str.Split(' ');
+List<int> ret = new List<int>();
+foreach(string term in terms){
+    ret.Add(term.Length);
+}
+foreach (var item in ret)
+{
+    Console.WriteLine(item);
+}
