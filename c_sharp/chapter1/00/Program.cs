513a8f16f3126ee7a05a8611af2793406d216087
s246wv <satoshi.nishimura.3194@gmail.com>
2021-11-22 12:45:32 +0900

    ループの回し方がスマートでない．

---
+++ c_sharp/chapter1/00/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+00. 文字列の逆順Permalink
+文字列”stressed”の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+*/
+string str = "stressed";
+char[] ret = new char[str.Length];
+for(int i=0;i<str.Length;i++){
+    ret[str.Length-i-1]=str[i];
+}
+Console.WriteLine(ret);
+
+
